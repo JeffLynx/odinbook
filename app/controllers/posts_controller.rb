8152c4b6756d4c74237b3708ed4fde2112b1bd3b
JeffLynx <josefloberg@gmail.com>
2026-02-17 19:42:00 +0100

    add create post form

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,30 @@
 class PostsController < ApplicationController
   def index
+    @post = Post.new
     @posts = Post
       .where(user: current_user.following + [ current_user ])
       .order(created_at: :desc)
       .includes(:user, :likes, :comments)
+  end
+
+  def create
+    @post = current_user.posts.build(post_params)
+
+    if @post.save
+      redirect_to posts_path, notice: "Posted!"
+    else
+      @posts = Post
+        .where(user: current_user.following + [ current_user ])
+        .order(created_at: :desc)
+        .includes(:user, :likes, :comments)
+
+      render :index, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:body)
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,5 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
+  validates :body, presence: true, length: { maximum: 230 }
 end
